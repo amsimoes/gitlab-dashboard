ad02697cc0b472cd2398e1f62c6e58775d5f7616
pcaseiro <pcaseiro@student.dei.uc.pt>
2016-09-13 21:52:10 +0100

    add's two ways of getting data from dei git

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask import request
+import requests
+import json
 import gitlab
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    gl = gitlab.Gitlab('https://git.dei.uc.pt/', '8fH8Vs4WNpYhVUBPzq5g')
+    gl.auth()
+    projects = gl.projects.list()
+    for project in projects:
+        print(project.branches.list())
+
+    # response = requests.get('https://git.dei.uc.pt/pcaseiro/ES-team-zig', headers={'PRIVATE-TOKEN': '8fH8Vs4WNpYhVUBPzq5g'})
+    # print(response).content
+    return "hello"
+
 
 if __name__ == '__main__':
     app.run()
