d9bb534f90200b20f3412926403e434ffa905b31
pcaseiro <pcaseiro@student.dei.uc.pt>
2016-09-15 14:30:46 +0100

    add's method to list all the files from a project

--- app.py
+++ app.py
@@ -6,22 +6,29 @@ import gitlab
 
 app = Flask(__name__)
 
+private_token = '8fH8Vs4WNpYhVUBPzq5g' 
+
+#TODO: method to get project ID
+#TODO: method that only makes requests, to avoid code duplication
+
 @app.route('/')
 def hello_world():
-    gl = gitlab.Gitlab('https://git.dei.uc.pt/', '8fH8Vs4WNpYhVUBPzq5g')
-    gl.auth()
-    #projects = gl.projects.list()
-    #for project in projects:
-    #    print(project.branches.list())
+    response = requests.get('https://git.dei.uc.pt/api/v3/projects?private_token={private_token}'.format(private_token=private_token))
 
-    response = requests.get('https://git.dei.uc.pt/api/v3/projects?private_token=8fH8Vs4WNpYhVUBPzq5g')
 
-    #print(json.load(response))
-    #return "hello"
-    return (response).content
-            
-    
+    print(response.json()[0]['default_branch'])
 
+    return response.content 
+
+
+@app.route('/projects')
+def list_project_files():
+    response = requests.get('https://git.dei.uc.pt/api/v3/projects/737/repository/tree?private_token={private_token}'.format(private_token=private_token))
+
+    for file in response.json():
+        print(file['name'])
+
+    return response.content
 
 if __name__ == '__main__':
     app.run()
