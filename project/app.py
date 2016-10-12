739fac39c12e20bf36482d79d73459f49b01027a
A Simoes <asimoes@student.dei.uc.pt>
2016-10-12 18:12:50 +0100

    Cleared some functions not working

--- project/app.py
+++ project/app.py
@@ -43,67 +43,6 @@ def parse_date(date):
     return data
 
 
-def get_file_diff(project_id, commit_id):
-    path = '/projects/{project_id}/repository/commits/{sha}/diff'.format(project_id=project_id, \
-            sha=commit_id)
-    response = make_get_request(path)
-
-    #print("Len dicionario: "+str(len(response.json())))
-
-    if len(str(response.content)) < 10: # Commit vazio
-        return 'nothing'
-    else:
-        if len(response.json()) > 1:   # Mais que um ficheiro por commit, retorna dicionario
-            file_names = []
-            l = response.json()
-            print(type(l[0]))
-            for i in range(len(l)):
-                f = l[i]['new_path']
-                if '/' in f:
-                    f = f.split('/')
-                    f = f[-1]
-                file_names.append(f)
-            return file_names
-        else:
-            f_name = response.json()[0]["new_path"]
-            if "/" in f_name:   # Ficheiro dentro de alguma pasta
-                print("Dentro de uma pasta...")
-                f_name = f_name.split("/")
-                f_name = f_name[-1]
-            return f_name
-
-
-def get_stats_per_file(file_name):
-    project_id = get_project_id()
-    path = '/projects/{project_id}/repository/commits?per_page=100'.format(project_id = project_id)
-    response = make_get_request(path)
-    commit_count = additions = deletions = 0
-    contributors = []
-    found = False
-
-    for commits in response.json():
-        res = get_file_diff(project_id, str(commits['id']))
-        if isinstance(res, list):
-            print(res)
-            for i in range(len(res)):
-                if res[i] == file_name:
-                    res = file_name
-                    break
-        if res == file_name:
-            if commits["author_name"] not in contributors:
-                contributors.append(commits["author_name"])
-            adds, dels = get_commit_stats(project_id, str(commits['id']))
-            commit_count += 1
-            additions += adds
-            deletions += dels
-            print("Additions: "+str(additions)+" Deletions: "+str(deletions))
-            creation_date = commits['created_at']
-            found = True
-    if not found:
-        return 0,0,0,0,0
-    return commit_count, additions, deletions, parse_date(creation_date), contributors
-
-
 # Invalid file_path -> retorna 0
 def get_blob_id(project_id, file_path):
     path = '/projects/{project_id}/repository/files?file_path={f}&ref=master'.format(project_id=project_id,f=file_path)
@@ -164,7 +103,7 @@ def list_project_files():
     return response.content
 
 
-# Precisa do id do ficheiro para mostrar o conteúdo (HTML n mostram o conteudo, mostra o produto)
+# Precisa do id do ficheiro para mostrar o conteúdo
 @app.route('/projects/files/content')
 def get_file_content():
     file_path = 'templates/hello.html'
@@ -207,21 +146,7 @@ def list_folder_files():
 
     return json.dumps(files)
 
-
-# Lista o numero de commits por ficheiro
-@app.route('/projects/files/commits')
-def list_file_stats():
-    file_name = ".gitignore"
-    commits, additions, deletions, creation_date, contributors = get_stats_per_file(file_name)
-    if commits == 0:
-        return "File {f} not commited yet.".format(f=file_name)
-    lista = ', '.join(contributors)
-
-    return "{f} | Creation date: {date} | \
-        {c} Commits {a} Additions {d} Deletions \
-        | Contributors: {l}".format(f=file_name,date=creation_date,c=commits,a=additions,d=deletions,l=lista)
-
-
+# Substituir pela funcao contributors?
 @app.route('/projects/members')
 def list_project_members():
     project_id = get_project_id()
