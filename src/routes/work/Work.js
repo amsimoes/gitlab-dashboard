8cbee7a30bd13447865359f000e78043da4f18ec
Carlos Pinho <kakasp04@gmail.com>
2016-11-08 09:08:08 +0000

    Work using AllCommits feature

--- src/routes/work/Work.js
+++ src/routes/work/Work.js
@@ -2,11 +2,12 @@ import React, { PropTypes  } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Work.css';
 import Header from '../../components/Header';
-
+import AllCommits from '../../components/AllCommits';
 function Work() {
   return (
     <div>
       <Header />
+      <AllCommits />
     </div>
   );
 }
