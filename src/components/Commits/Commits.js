fe407ed0e96a918f23a7e313eda8308ef7f8df0e
pedrocaseiro <pcaseiro96@gmail.com>
2016-11-14 14:33:08 +0000

    creates commits graph for each person

--- src/components/Commits/Commits.js
+++ src/components/Commits/Commits.js
@@ -8,49 +8,91 @@ import Email from '../ProjectName';
 import Additionsn from '../Navigation';
 import Deletions from '../Link';
 
-class Commits extends Component {
+var BarChart = require("react-chartjs").Bar;
 
+class Commits extends Component {
   constructor(props) {
     super(props);
     this.state = {
       contributors: '',
+      check: false,
     };
-  }
+    this.chartData = {
+      labels: [],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+
+          ],
+          borderWidth: 1,
+          data: [],
+        }
+      ]
+    };
+    this.chartOptions = {
+      scales: {
+        xAxes: [{
+          stacked: true
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    };
+   }
 
   componentWillMount = () => {
     axios.get('http://localhost:5000/projects/contributors')
       .then(function (response) {
-        console.log(response.data);
         this.setState({contributors: response.data});
       }.bind(this))
       .catch(function (error) {
         console.log(error);
-
       });
-
   }
 
   render() {
     if(this.state.contributors){
       return(
-       <div >
+        <div>
+            <div className={s.graphic}><BarChart data={this.chartData} options={this.chartOptions} width="600" height="250"/></div>
+          <div>
           {Object.keys(this.state.contributors).map(function(key) {
+            this.chartData.datasets[0].data.push(this.state.contributors[key].commits);
+            this.chartData.labels.push(this.state.contributors[key].name);
             return(
               <div className={cx(s.table, s.lines)}>
-                <div >Name: {this.state.contributors[key].name}</div>
+                <div>Name: {this.state.contributors[key].name}</div>
                 <div>Email: {this.state.contributors[key].email}</div>
                 <div>Commits: {this.state.contributors[key].commits}</div>
                 <div>Additions: {this.state.contributors[key].additions}</div>
                 <div>Deletions: {this.state.contributors[key].deletions}</div>
               </div>
-            );
-          }.bind(this))}
-        </div>
+          );
+        }.bind(this))}
+            </div>
+          </div>
       );
     } else {
       return(
-       <div className={s.loading_style}>
-        Loading
+        <div className={s.loading_style}>
+        Loading Members Contributions
         </div>
       );
     }
