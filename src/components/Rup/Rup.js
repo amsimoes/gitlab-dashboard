faa1967732c1b6f7a82aa88ef7a5ce514c6b911a
Carlos Pinho <kakasp04@gmail.com>
2016-11-28 14:09:20 +0000

    remove get request from component

--- src/components/Rup/Rup.js
+++ src/components/Rup/Rup.js
@@ -6,16 +6,6 @@ import cx from 'classnames';
 
 class Rup extends Component {
 
-  componentWillMount = () => {
-    axios.get('http://localhost:5000/processes')
-      .then(function(response){
-        console.log("123");
-      }.bind(this))
-      .catch(function(error){
-        console.log(error);
-      });
-  }
-
   render() {
     return(
       <ReactRedirect location='http://localhost:8000/effortTimeline.html'></ReactRedirect>
