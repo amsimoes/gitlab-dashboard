51b76f8d42368e953c9dea131030643041a196ea
Maria Margarida <mariamargarida@MacBook-Pro-de-Maria-8.local>
2016-12-12 14:04:46 +0000

    dzain

--- src/components/Html.js
+++ src/components/Html.js
@@ -20,6 +20,8 @@ function Html({ title, description, style, script, children }) {
         <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="imgs/favicon.png" />
+        <link rel="stylesheet" href="css/font-awesome.min.css"/>
         {style && <style id="css" dangerouslySetInnerHTML={{ __html: style }} />}
       </head>
       <body>
